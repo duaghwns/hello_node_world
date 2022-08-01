4503293969ed02ce6297d278cee3e2c4f5bba157
duaghwns <hj2758@gmail.com>
2022-08-01 16:40:34 +0900

    Todo list

--- server.js
+++ server.js
@@ -10,37 +10,64 @@ const _dirPages = __dirname + '/pages/'
 const port = 8080;
 const mongoUrl = 'mongodb+srv://root:root@cluster0.n5blm.mongodb.net/?retryWrites=true&w=majority'
 // ejs 사용
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 let db;
 let saveUser;
+let saveTodo;
 
-MongoClient.connect(mongoUrl, (err, client) => {
-    if(err) return console.log(err);
-    
-    db = client.db('todoapp');
-    
-    saveUser = (userObj)=>{
-        db.collection('user').insertOne(userObj, (err,result) => {
-            if(err) return console.log(err);
-            console.log('저장완료')
-            console.log(result);
-        });
-    }
-})
+    MongoClient.connect(mongoUrl, (err, client) => {
+        if(err) return console.log(err);
+        
+        db = client.db('todoapp');
+        
+        saveUser = obj => {
+            db.collection('user').insertOne(obj, (err,result) => {
+                if(err) return console.log(err);
+                console.log('저장완료')
+                console.log(result);
+            });
+        }
 
-app.listen(port, ()=> {
-    console.log(`listening on port ${port}`)
-});
-app.use(bodyParser.urlencoded({ extended : true }));
+        saveTodo = obj => {
+            db.collection('post').insertOne(obj, (err,result) => {
+                if(err) return console.log(err)
+                console.log('save!')
+                console.log(result)
+            });
+        }
+    })
+
+    app.listen(port, ()=> {
+        console.log(`listening on port ${port}`)
+    });
+    app.use(bodyParser.urlencoded({ extended : true }));
     
     
     app.get('/', (req, res) => {
-        res.sendFile(_dirPages + 'index.html')
+
+        let postData = db.collection('post').find().toArray((e,data) => {
+            if(e) return console.log(e)
+            console.log(data)
+            res.render('index.ejs', {posts : data})
+        })
+
+
     })
     
     app.get('/login', (req, res) => {
         res.sendFile(_dirPages + 'login.html')
+    })
+
+    app.get('/todo', (req, res) => {
+
+        let postData = db.collection('post').find().toArray((err, data) => {
+            if(err) return console.log(err);
+
+            console.log(data);
+        });
+
+        res.render('todo.ejs')
     })
     
     app.post('/login', (req, res) => {
@@ -69,4 +96,18 @@ app.use(bodyParser.urlencoded({ extended : true }));
         if(req.body != null){
             saveUser(user);
         }
+
+        res.sendFile(_dirPages + 'login.html')
+    })
+
+    app.post('/todo', (req, res)=>{
+        const todo = {
+            title: req.body.title,
+            content : req.body.content,
+            date : req.body.date
+        }
+
+        saveTodo(todo);
+
+        res.sendFile(_dirPages + 'index.html')
     })
